20af0d8f08200fefc95dc398cb3e6c4222939134
3mmar0 <ammarelgndy6@gmail.com>
2023-12-19 22:01:49 +0200

    notification

--- app/Models/Admin/Store.php
+++ app/Models/Admin/Store.php
@@ -5,10 +5,11 @@ namespace App\Models\Admin;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Store extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
